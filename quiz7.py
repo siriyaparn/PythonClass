ef386218f2b2714be259fae97518a609a3e87a2a
siriyaparn <90504906+siriyaparn@users.noreply.github.com>
2022-08-04 22:01:12 +0700

    Create a Matrix X4x3

---
+++ quiz7.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+M = np.array([[1, 2, 3],[4, 5, 6],[7, 8, 9],[10, 11, 12], [13, 14, 15]])
+print(M[2,:]) # show only the matrix at position 2 which is [7 8 9]
+print(M[2:])  # show matrixs from position 2 to the last matrix
