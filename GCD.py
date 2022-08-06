c145775cd1494f1e6e7210d88d48f4d23027ddf2
siriyaparn <90504906+siriyaparn@users.noreply.github.com>
2022-08-06 21:59:11 +0700

    The greatest common divisor

---
+++ GCD.py
@@ -0,0 +1,12 @@
+first = int(input("Please input first number : "))
+second = int(input("Please input second number : "))
+if first < second :
+  min = first
+else :
+  min = second
+#print("Check min number" ,min)
+
+for i in range(1, min) :
+  if first%i == 0 and second%i == 0 :
+    result = i
+print("The greatest common divisor is ", result)
