8ab13547d58590fca0af7df7b663de099d16b8d1
siriyaparn <siriyasriboonma@gmail.com>
2022-08-30 14:30:25 +0700

    Reverse range : start dy decreasing

--- GCD.py
+++ GCD.py
@@ -6,7 +6,8 @@ else :
   min = second
 #print("Check min number" ,min)
 
-for i in range(1, min+1) :
+for i in range(min,2,-1) :
   if first%i == 0 and second%i == 0 :
     result = i
+    break
 print("The greatest common divisor is ", result)
